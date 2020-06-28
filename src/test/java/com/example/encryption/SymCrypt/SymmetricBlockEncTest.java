49ad9cd81c38d32c4047a3a5b0e5d9a43ba49f73
hugebawu <drbjhu@163.com>
2020-06-28 14:39:19 +0800

    modify SymmetricBlockEncTest.java

--- src/test/java/com/example/encryption/SymCrypt/SymmetricBlockEncTest.java
+++ src/test/java/com/example/encryption/SymCrypt/SymmetricBlockEncTest.java
@@ -9,13 +9,23 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.bouncycastle.crypto.DataLengthException;
+import org.bouncycastle.crypto.InvalidCipherTextException;
 import org.bouncycastle.util.encoders.Hex;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.edu.ncepu.crypto.encryption.SymCrypt.SymmetricBlockEnc;
 import cn.edu.ncepu.crypto.encryption.SymCrypt.SymmetricBlockEnc.Mode;
@@ -31,80 +41,86 @@ import edu.princeton.cs.algs4.StdOut;
  * @Description: TODO(test the symmetric block encryption algorithm)
  */
 public class SymmetricBlockEncTest {
+	private static Logger logger = LoggerFactory.getLogger(SymmetricBlockEncTest.class);
 
 	@Ignore
 	@Test
 	public void testAESString() {
-		final String key = "6206c34e2186e752c74e6df32ab8fa5b";
-		final String iv = "00e5d201c2c2acbff8154861242ba0c4";
-		final String iv_p = "00e5d201c2c2acbff8154861242ba0c5";
-		String message;
-		byte[] ciphertext, ciphertext_p;
-		String plaintext, plaintext_p;
-
-		// Test ECB Mode
-		StdOut.println("Test AES with ECB Mode.");
-		message = "Message";
-		StdOut.println("Message = " + message);
-		ciphertext = SymmetricBlockEnc.enc_AES(Mode.ECB, Hex.decode(key), null, message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
-		plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.ECB, Hex.decode(key), null, ciphertext));
-		StdOut.println("Decrypted Plaintext = " + plaintext);
-		StdOut.println();
-
-		// Test CBC Mode
-		StdOut.println("Test AES with CBC Mode.");
-		message = "Message";
-		StdOut.println("Message = " + message);
-		// Test for Correctness
-		ciphertext = SymmetricBlockEnc.enc_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv), message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
-		plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv), ciphertext));
-		StdOut.println("Decrypted Plaintext = " + plaintext);
-		// Test for Encryption with distinct IV
-		ciphertext_p = SymmetricBlockEnc.enc_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv_p), message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext_p));
-		plaintext_p = new String(SymmetricBlockEnc.dec_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
-		StdOut.println("Decrypted Plaintext = " + plaintext_p);
-		StdOut.println();
-
-		// Test CFB Mode
-		StdOut.println("Test AES with CFB Mode.");
-		message = "Message";
-		StdOut.println("Message = " + message);
-		// Test for Correctness
-		ciphertext = SymmetricBlockEnc.enc_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv), message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
-		plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv), ciphertext));
-		StdOut.println("Decrypted Plaintext = " + plaintext);
-		// Test for Encryption with distinct IV
-		ciphertext_p = SymmetricBlockEnc.enc_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv_p), message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext_p));
-		plaintext_p = new String(SymmetricBlockEnc.dec_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
-		StdOut.println("Decrypted Plaintext = " + plaintext_p);
-		StdOut.println();
-
-		// Test OFB Mode
-		StdOut.println("Test AES with OFB Mode.");
-		message = "Message";
-		StdOut.println("Message = " + message);
-		// Test for Correctness
-		ciphertext = SymmetricBlockEnc.enc_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv), message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
-		plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv), ciphertext));
-		StdOut.println("Decrypted Plaintext = " + plaintext);
-		// Test for Encryption with distinct IV
-		ciphertext_p = SymmetricBlockEnc.enc_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv_p), message.getBytes());
-		StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext_p));
-		plaintext_p = new String(SymmetricBlockEnc.dec_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
-		StdOut.println("Decrypted Plaintext = " + plaintext_p);
-		StdOut.println();
-
-		StdOut.println("Test AES with CBC Mode realized through BouncyCastle.");
-		message = "Message";
-		StdOut.println("Message = " + message);
-		// Test for Correctness
 		try {
+			final String key = "6206c34e2186e752c74e6df32ab8fa5b";
+			final String iv = "00e5d201c2c2acbff8154861242ba0c4";
+			final String iv_p = "00e5d201c2c2acbff8154861242ba0c5";
+			String message;
+			byte[] ciphertext, ciphertext_p;
+			String plaintext, plaintext_p;
+
+			// Test ECB Mode
+			StdOut.println("Test AES with ECB Mode.");
+			message = "Message";
+			StdOut.println("Message = " + message);
+			ciphertext = SymmetricBlockEnc.enc_AES(Mode.ECB, Hex.decode(key), null, message.getBytes());
+
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
+			plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.ECB, Hex.decode(key), null, ciphertext));
+			StdOut.println("Decrypted Plaintext = " + plaintext);
+			StdOut.println();
+
+			// Test CBC Mode
+			StdOut.println("Test AES with CBC Mode.");
+			message = "Message";
+			StdOut.println("Message = " + message);
+			// Test for Correctness
+			ciphertext = SymmetricBlockEnc.enc_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv), message.getBytes());
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
+			plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv), ciphertext));
+			StdOut.println("Decrypted Plaintext = " + plaintext);
+			// Test for Encryption with distinct IV
+			ciphertext_p = SymmetricBlockEnc.enc_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv_p), message.getBytes());
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext_p));
+			plaintext_p = new String(
+					SymmetricBlockEnc.dec_AES(Mode.CBC, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
+			StdOut.println("Decrypted Plaintext = " + plaintext_p);
+			StdOut.println();
+
+			// Test CFB Mode
+			StdOut.println("Test AES with CFB Mode.");
+			message = "Message";
+			StdOut.println("Message = " + message);
+			// Test for Correctness
+			ciphertext = SymmetricBlockEnc.enc_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv), message.getBytes());
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
+			plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv), ciphertext));
+			StdOut.println("Decrypted Plaintext = " + plaintext);
+			// Test for Encryption with distinct IV
+			ciphertext_p = SymmetricBlockEnc.enc_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv_p), message.getBytes());
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext_p));
+			plaintext_p = new String(
+					SymmetricBlockEnc.dec_AES(Mode.CFB, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
+			StdOut.println("Decrypted Plaintext = " + plaintext_p);
+			StdOut.println();
+
+			// Test OFB Mode
+			StdOut.println("Test AES with OFB Mode.");
+			message = "Message";
+			StdOut.println("Message = " + message);
+			// Test for Correctness
+			ciphertext = SymmetricBlockEnc.enc_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv), message.getBytes());
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext));
+			plaintext = new String(SymmetricBlockEnc.dec_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv), ciphertext));
+			StdOut.println("Decrypted Plaintext = " + plaintext);
+			// Test for Encryption with distinct IV
+			ciphertext_p = SymmetricBlockEnc.enc_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv_p), message.getBytes());
+			StdOut.println("Encrypted Ciphertext = " + Hex.toHexString(ciphertext_p));
+			plaintext_p = new String(
+					SymmetricBlockEnc.dec_AES(Mode.OFB, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
+			StdOut.println("Decrypted Plaintext = " + plaintext_p);
+			StdOut.println();
+
+			StdOut.println("Test AES with CBC Mode realized through BouncyCastle.");
+			message = "Message";
+			StdOut.println("Message = " + message);
+
+			// Test for Correctness
 			// sender send [IV,ciphertext] to receiver
 			// generate random 16 bytes initialization vector
 			SecureRandom sr = SecureRandom.getInstanceStrong();
@@ -130,10 +146,22 @@ public class SymmetricBlockEncTest {
 					SymmetricBlockEnc.enc_dec_AES_BC(false, Mode.CBC, Hex.decode(key), Hex.decode(iv_p), ciphertext_p));
 			StdOut.println("Decrypted Plaintext = " + plaintext_p);
 			StdOut.println();
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+		} catch (DataLengthException | IllegalStateException | InvalidCipherTextException e) {
+			logger.error(e.getLocalizedMessage());
 		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
+			logger.error(e.getLocalizedMessage());
+		} catch (InvalidKeyException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (NoSuchPaddingException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (InvalidAlgorithmParameterException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (IllegalBlockSizeException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (BadPaddingException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (UnsupportedEncodingException e) {
+			logger.error(e.getLocalizedMessage());
 		}
 	}
 
@@ -160,9 +188,15 @@ public class SymmetricBlockEncTest {
 			in.close();
 			out.close();
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			logger.error(e.getLocalizedMessage());
 		} catch (IOException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (DataLengthException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (IllegalStateException e) {
 			e.printStackTrace();
+		} catch (InvalidCipherTextException e) {
+			logger.error(e.getLocalizedMessage());
 		}
 	}
 }

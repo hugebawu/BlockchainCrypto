743a3a117d161642071a62380c81437f07af9109
hugebawu <drbjhu@163.com>
2020-06-28 13:30:41 +0800

    modify DH.java

--- src/main/java/cn/edu/ncepu/crypto/keyExchange/DH.java
+++ src/main/java/cn/edu/ncepu/crypto/keyExchange/DH.java
@@ -3,11 +3,7 @@
  */
 package cn.edu.ncepu.crypto.keyExchange;
 
-import static org.junit.Assert.assertEquals;
-
-import java.security.GeneralSecurityException;
 import java.security.InvalidKeyException;
-import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -15,12 +11,13 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
-import java.security.spec.X509EncodedKeySpec;
-import java.util.Base64;
 
 import javax.crypto.KeyAgreement;
 
+import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
+
+import cn.edu.ncepu.crypto.utils.CommonUtils;
 
 /**
  * @Copyright : Copyright (c) 2020-2021 
@@ -34,58 +31,74 @@ import org.apache.commons.codec.binary.Hex;
 public class DH {
 	private static final String DH_STRING = "DH";
 
-	public static KeyPair getDHKeyPair(int keysize) {
+	/**
+	 * TODO get Diffie_Hellman algorithm key pair
+	 * @param keysize
+	 * @return
+	 * @throws NoSuchAlgorithmException 
+	 */
+	public static KeyPair getDHKeyPair(int keysize) throws NoSuchAlgorithmException {
 		// The valid key length should be the multiple of 64 bits, default = 1024 bits.
-		assertEquals(0, keysize % 64);
-		try {
-			KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(DH_STRING);
-			SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-			// set key parameters
-			keyPairGenerator.initialize(keysize, random);
-			return keyPairGenerator.generateKeyPair();
-		} catch (GeneralSecurityException e) {
-			e.printStackTrace();
+		if (0 != keysize % 64) {
+			throw new IllegalArgumentException(
+					"valid key length should be the multiple of 64 bits, and minumum secure is 1024");
 		}
-		return null;
+		KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(DH_STRING);
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
+		// set key parameters
+		keyPairGenerator.initialize(keysize, random);
+		return keyPairGenerator.generateKeyPair();
 	}
 
 	/**
-	 * @Description: TODO(generate shared key from public and private String key)
-	 * @param publicKey String
-	 * @param PrivateKey String
-	 * @return shared key string
-	 * @throws
-	 */
-//	public static String genSharedKey(String publicKey, String privateKey) {
-//		return genSharedKey((PublicKey) ECUtils.string2ECKey(true, publicKey),
-//				(PrivateKey) ECUtils.string2ECKey(false, privateKey));
-//	}
-
-	/**
-	 * @Description: TODO(generate shared key for ECDH key exchange scheme)
+	 * TODO(generate shared key for ECDH key exchange scheme)
 	 * @param publickey: the other party's based64 encoded Diffie-Hellman public key
 	 * @param privateKey: own private key
 	 * @return shared key string
-	 * @throws
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
 	 */
-	public static String genSharedKey(String publickey, PrivateKey privateKey) {
-		try {
-			// recover PublicKey from Base64 encoded public key
-			// PublicKey is encoded according to X.509 protocol
-			X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.getDecoder().decode(publickey));
-			KeyFactory kf = KeyFactory.getInstance(DH_STRING);
-			PublicKey publicKey = kf.generatePublic(keySpec);
-			KeyAgreement keyAgreement = KeyAgreement.getInstance(DH_STRING);
-			keyAgreement.init(privateKey);
-			keyAgreement.doPhase(publicKey, true);
-			return Hex.encodeHexString(keyAgreement.generateSecret());
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (InvalidKeySpecException e) {
-			e.printStackTrace();
-		}
-		return null;
+	public static String genSharedKey(PublicKey publickey, PrivateKey privatekey)
+			throws NoSuchAlgorithmException, InvalidKeyException {
+		KeyAgreement keyAgreement = KeyAgreement.getInstance(DH_STRING);
+		keyAgreement.init(privatekey);
+		keyAgreement.doPhase(publickey, true);
+		return Hex.encodeHexString(keyAgreement.generateSecret());
 	}
+
+	/**
+	 * TODO(generate shared key for ECDH key exchange scheme)
+	 * @param publickey: the other party's based64 encoded Diffie-Hellman public key
+	 * @param privateKey: own private key
+	 * @return shared key string
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
+	 * @throws DecoderException 
+	 * @throws InvalidKeySpecException 
+	 */
+	public static String genSharedKey(String publickey, PrivateKey privatekey)
+			throws NoSuchAlgorithmException, InvalidKeyException, InvalidKeySpecException, DecoderException {
+		KeyAgreement keyAgreement = KeyAgreement.getInstance(DH_STRING);
+		keyAgreement.init(privatekey);
+		PublicKey publicKey = (PublicKey) CommonUtils.string2ECKey(true, "Base64", publickey, DH_STRING);
+		keyAgreement.doPhase(publicKey, true);
+		return Hex.encodeHexString(keyAgreement.generateSecret());
+	}
+
+	/**
+	 * TODO(generate shared key from public and private String key)
+	 * @param publicKey String
+	 * @param PrivateKey String
+	 * @return shared key string
+	 * @throws NoSuchAlgorithmException 
+	 * @throws InvalidKeyException 
+	 * @throws DecoderException 
+	 * @throws InvalidKeySpecException 
+	 */
+	public static String genSharedKey(String publicKey, String privateKey)
+			throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, DecoderException {
+		return genSharedKey((PublicKey) CommonUtils.string2ECKey(true, "Base64", publicKey, DH_STRING),
+				(PrivateKey) CommonUtils.string2ECKey(false, "Base64", privateKey, DH_STRING));
+	}
+
 }

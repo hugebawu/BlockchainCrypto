21d5f73826fa753955087790293e07a94b33f92a
hugebawu <drbjhu@163.com>
2020-06-28 14:34:15 +0800

    modify ECIESEngineJUniteTest.java

--- src/test/java/com/example/encryption/ecies/ECIESEngineJUniteTest.java
+++ src/test/java/com/example/encryption/ecies/ECIESEngineJUniteTest.java
@@ -5,13 +5,23 @@ package com.example.encryption.ecies;
 
 import static org.junit.Assert.assertEquals;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.edu.ncepu.crypto.encryption.ecies.ECIESEngine;
-import cn.edu.ncepu.crypto.utils.ECUtils;
+import cn.edu.ncepu.crypto.utils.CommonUtils;
+import cn.edu.ncepu.crypto.utils.SysProperty;
 
 /**
  * @Copyright : Copyright (c) 2020-2021 
@@ -23,28 +33,34 @@ import cn.edu.ncepu.crypto.utils.ECUtils;
  * @Description: TODO(elliptic curve integrated encryption scheme test)
  */
 public class ECIESEngineJUniteTest {
+	private static Logger logger = LoggerFactory.getLogger(ECIESEngineJUniteTest.class);
+	private static String USER_DIR = SysProperty.USER_DIR;
+	private static final String EC_STRING = "EC";
 
-//	@Ignore
+	@Ignore
 	@Test
 	public void testECIES_Encrypt_Eecrypt() {
-		PublicKey publicKey = null;
-		PrivateKey privateKey = null;
 		try {
-			publicKey = (PublicKey) ECUtils.loadECKeyFromPEM(true, "src/test/java/com/example/utils/publicKey.pem");
-			privateKey = (PrivateKey) ECUtils.loadECKeyFromPEM(false, "src/test/java/com/example/utils/privateKey.pem");
+			PublicKey publicKey = null;
+			PrivateKey privateKey = null;
+			publicKey = (PublicKey) CommonUtils.loadKeyFromPEM(true, EC_STRING, USER_DIR + "/elements/publicKey.pem");
+			privateKey = (PrivateKey) CommonUtils.loadKeyFromPEM(false, EC_STRING,
+					USER_DIR + "/elements/privateKey.pem");
+			String content = "cryptography12342qer45taredfghdfghj/？！#@￥##%……";
+			// encrypt the ciphertext can be transmitted directly through network.
+			String ciphertext = ECIESEngine.encrypt(content, publicKey);
+			System.out.println("plaintext: " + content);
+			System.out.println("base64 ciphertext: " + ciphertext);
+			System.out.println("base64 ciphertext length: " + ciphertext.length());
+			// decrypt
+			String decryptedtext = ECIESEngine.decrypt(ciphertext, privateKey);
+			System.out.println("decrypted plaintext: " + decryptedtext);
+			assertEquals(content, decryptedtext);
+		} catch (InvalidKeyException | NoSuchAlgorithmException | NoSuchPaddingException | IllegalBlockSizeException
+				| BadPaddingException e) {
+			logger.error(e.getLocalizedMessage());
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			logger.error(e.getLocalizedMessage());
 		}
-		String content = "cryptography12342qer45taredfghdfghj/？！#@￥##%……";
-		// encrypt the ciphertext can be transmitted directly through network.
-		String ciphertext = ECIESEngine.encrypt(content, publicKey);
-		System.out.println("plaintext: " + content);
-		System.out.println("base64 ciphertext: " + ciphertext);
-		System.out.println("base64 ciphertext length: " + ciphertext.length());
-		// decrypt
-		String decryptedtext = ECIESEngine.decrypt(ciphertext, privateKey);
-		System.out.println("decrypted plaintext: " + decryptedtext);
-		assertEquals(content, decryptedtext);
 	}
 }

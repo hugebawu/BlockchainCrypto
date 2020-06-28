bfee9d4d77edbe1d2b64a5588fe443f3e184d9b1
hugebawu <drbjhu@163.com>
2020-06-28 14:35:35 +0800

    modify RSAEncEngineTest.java

--- src/test/java/com/example/encryption/rsa/RSAEncEngineTest.java
+++ src/test/java/com/example/encryption/rsa/RSAEncEngineTest.java
@@ -6,12 +6,22 @@ package com.example.encryption.rsa;
 import static org.junit.Assert.assertEquals;
 
 import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.junit.Ignore;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import cn.edu.ncepu.crypto.encryption.rsa.RSAEncEngine;
 
@@ -25,29 +35,29 @@ import cn.edu.ncepu.crypto.encryption.rsa.RSAEncEngine;
  * @Description: TODO(RSA encryption scheme)
  */
 public class RSAEncEngineTest {
+	private static Logger logger = LoggerFactory.getLogger(RSAEncEngineTest.class);
 
-//	@Ignore
+	@Ignore
 	@Test
 	/**
-	 * @Description: TODO(test RSA encryption and decryption method)
+	 * TODO(test RSA encryption and decryption method)
 	 * @throws
 	 */
 	public void test_enc_dec_RSA() {
-		System.out.println("Testing RSA encryption scheme scheme.");
-		int keysize = 3072;
-		// generate key pair
-		KeyPair keyPair = RSAEncEngine.getRSAKeyPair(keysize);
-		PublicKey publicKey = keyPair.getPublic();
-		PrivateKey privateKey = keyPair.getPrivate();
-
-		String message = "Message";
-		System.out.println("Message: " + message);
-
-		// encrypt the base64 ciphertext can be transmitted directly through network.
-		String base64_publciKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
-		byte[] encryptedtext;
 		try {
-			encryptedtext = RSAEncEngine.encrypt(message.getBytes("UTF-8"), base64_publciKey);
+			System.out.println("Testing RSA encryption scheme scheme.");
+			int keysize = 3072;
+			// generate key pair
+			KeyPair keyPair = RSAEncEngine.getRSAKeyPair(keysize);
+			PublicKey publicKey = keyPair.getPublic();
+			PrivateKey privateKey = keyPair.getPrivate();
+
+			String message = "Message";
+			System.out.println("Message: " + message);
+
+			// encrypt the base64 ciphertext can be transmitted directly through network.
+			String base64_publciKey = Base64.getEncoder().encodeToString(publicKey.getEncoded());
+			byte[] encryptedtext = RSAEncEngine.encrypt(message.getBytes("UTF-8"), base64_publciKey);
 			String ciphertext = Base64.getEncoder().encodeToString(encryptedtext);
 			System.out.println("base64 ciphertext: " + ciphertext);
 			System.out.println("base64 ciphertext length: " + ciphertext.length());
@@ -59,7 +69,19 @@ public class RSAEncEngineTest {
 			System.out.println("decrypted plaintext: " + decryptString);
 			assertEquals(message, decryptedtext);
 		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
+			logger.error(e.getLocalizedMessage());
+		} catch (NoSuchAlgorithmException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (InvalidKeyException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (InvalidKeySpecException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (NoSuchPaddingException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (IllegalBlockSizeException e) {
+			logger.error(e.getLocalizedMessage());
+		} catch (BadPaddingException e) {
+			logger.error(e.getLocalizedMessage());
 		}
 	}
 }

ee58cf443df4670e83081b98b4d18f64a4b696db
hugebawu <drbjhu@163.com>
2020-06-28 13:20:56 +0800

    modify ECIESEngine.java

--- src/main/java/cn/edu/ncepu/crypto/encryption/ecies/ECIESEngine.java
+++ src/main/java/cn/edu/ncepu/crypto/encryption/ecies/ECIESEngine.java
@@ -33,28 +33,18 @@ public class ECIESEngine {
 	 * @param content content waits to be encrypted
 	 * @param publicKey EC publicKey
 	 * @return Based64 coded ciphertext to convenient network transmission.
+	 * @throws IllegalBlockSizeException 
+	 * @throws InvalidKeyException 
+	 * @throws NoSuchPaddingException 
 	 */
-	public static String encrypt(String content, PublicKey publicKey) {
-		String cipherString = null;
-		try {
-			BouncyCastleProvider bcProvider = new BouncyCastleProvider();
-			Cipher cipher = Cipher.getInstance("ECIES", bcProvider);
-			cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-			byte[] ciphertext = cipher.doFinal(content.getBytes());
-			// for transmission encode cipherText as Base64
-			cipherString = Base64.getEncoder().encodeToString(ciphertext);
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		}
-		return cipherString;
+	public static String encrypt(String content, PublicKey publicKey) throws NoSuchAlgorithmException,
+			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		BouncyCastleProvider bcProvider = new BouncyCastleProvider();
+		Cipher cipher = Cipher.getInstance("ECIES", bcProvider);
+		cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		byte[] ciphertext = cipher.doFinal(content.getBytes());
+		// for transmission encode cipherText as Base64
+		return Base64.getEncoder().encodeToString(ciphertext);
 	}
 
 	/**
@@ -62,25 +52,17 @@ public class ECIESEngine {
 	 * @param ciphertext ciphertext waits to be encrypted
 	 * @param privateKey EC privateKey
 	 * @return original plaintext string.
+	 * @throws NoSuchPaddingException 
+	 * @throws NoSuchAlgorithmException 
+	 * @throws BadPaddingException 
+	 * @throws IllegalBlockSizeException 
+	 * @throws InvalidKeyException 
 	 */
-	public static String decrypt(String ciphertext, PrivateKey privateKey) {
-		String plaintext = null;
+	public static String decrypt(String ciphertext, PrivateKey privateKey) throws NoSuchAlgorithmException,
+			NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
 		BouncyCastleProvider bcProvider = new BouncyCastleProvider();
-		try {
-			Cipher cipher = Cipher.getInstance("ECIES", bcProvider);
-			cipher.init(Cipher.DECRYPT_MODE, privateKey);
-			plaintext = new String(cipher.doFinal(Base64.getDecoder().decode(ciphertext)));
-		} catch (NoSuchAlgorithmException e) {
-			e.printStackTrace();
-		} catch (NoSuchPaddingException e) {
-			e.printStackTrace();
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} catch (IllegalBlockSizeException e) {
-			e.printStackTrace();
-		} catch (BadPaddingException e) {
-			e.printStackTrace();
-		}
-		return plaintext;
+		Cipher cipher = Cipher.getInstance("ECIES", bcProvider);
+		cipher.init(Cipher.DECRYPT_MODE, privateKey);
+		return new String(cipher.doFinal(Base64.getDecoder().decode(ciphertext)));
 	}
 }
